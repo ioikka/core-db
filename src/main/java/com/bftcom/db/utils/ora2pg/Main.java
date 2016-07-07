c929f848c707083ae1e79813614bd6e09bc18bb2
p.shapoval <p.shapoval@bftcom.com>
2016-07-07 18:59:22 +0300

    added config properties

--- src/main/java/com/bftcom/db/utils/ora2pg/Main.java
+++ src/main/java/com/bftcom/db/utils/ora2pg/Main.java
@@ -44,16 +44,36 @@ public class Main {
 
       DbExport dbExport = new DbExport(srcPlatform);
       String[] tNames = tableNames.toArray(new String[0]);
+      
+      //Move data
+      if ("true".equals(System.getProperty("export.to.csv"))) {
+        Utils.exportToCsv(dbExport, tNames);//TO_CSV
+      }
+      if ("true".equals(System.getProperty("import.from.csv"))) {
+        Utils.copyData(excludedTablesList, dstPlatform, tNames);//FROM_CSV
+      }
 
-      Utils.exportToCsv(dbExport, tNames);//TO_CSV
-      Utils.copyData(excludedTablesList, dstPlatform, tNames);//FROM_CSV
-
-      Utils.createSequences(srcPlatform, dstPlatform);//SEQUENCES
-      Utils.createViews(srcPlatform, dstPlatform);//VIEWS
-      Utils.createUQ(srcPlatform, dstPlatform);//UQ
-      Utils.createFK(srcPlatform, dstPlatform);//FK
-      Utils.createCheck(srcPlatform, dstPlatform);//CHECK
-      Utils.convertBlobs(dstPlatform, dstTables);//BLOBS
+      //create constraints. there were not copied during tables creation phase
+      if ("true".equals(System.getProperty("dst.create.sequences"))) {
+        Utils.createSequences(srcPlatform, dstPlatform);//SEQUENCES
+      }
+      if ("true".equals(System.getProperty("dst.convert.views"))) {
+        Utils.createViews(srcPlatform, dstPlatform);//VIEWS
+      }
+      if ("true".equals(System.getProperty("dst.create.uqs"))) {
+        Utils.createUQ(srcPlatform, dstPlatform);//UQ
+      }
+      if ("true".equals(System.getProperty("dst.create.fks"))) {
+        Utils.createFK(srcPlatform, dstPlatform);//FK
+      }
+      if ("true".equals(System.getProperty("dst.create.checks"))) {
+        Utils.createCheck(srcPlatform, dstPlatform);//CHECK
+      }
+      
+      //convert blobs/ should be executed once
+      if ("true".equals(System.getProperty("dst.convert.blobs"))) {
+        Utils.convertBlobs(dstPlatform, dstTables);//BLOBS
+      }
 
     } catch (Exception e) {
       logger.error("", e);
